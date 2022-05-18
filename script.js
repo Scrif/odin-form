329a70aedbb0575081ab7799bf3b2feaba352f1d
scrif <andrews.austin.94@gmail.com>
2022-05-18 15:18:26 -0500

    CSS changes

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
-const pw = document.querySelector("#password");
-const pwC = document.querySelector("#passwordConfirm");
-const firstName = document.querySelector("#firstname");
-const lastName = document.querySelector("#lastname");
-const email = document.querySelector("#email");
-const tel = document.querySelector("#phonenumber");
-const pwError = document.querySelector("#password-confirm-error");
-const fnError = document.querySelector("#first-name-error");
-const lnError = document.querySelector("#last-name-error");
-const emailError = document.querySelector("#email-error");
-const telError = document.querySelector("#tel-error");
-const pwConfirmError = document.querySelector("#password-confirm-error");
-
-firstName.addEventListener("input", function (event) {
-    if (firstName.value === "") {
-    fnError.textContent = "Please enter your first name.";
-    } else {
-        fnError.textContent = "";
-    }
-});
-
-lastName.addEventListener("input", function (event) {
-    if (lastName.value === "") {
-    lnError.textContent = "Please enter your last name.";
-    } else {
-        lnError.textContent = "";
-    }
-});
-
-tel.addEventListener("input", function (event) {
-    if (tel.validity.patternMismatch) {
-    telError.textContent = "Please enter a 10 digit phone number.";
-    } else {
-        telError.textContent = "";
-    }
-});
-
-email.addEventListener("input", function (event) {
-    if (email.validity.typeMismatch) {
-    emailError.textContent = "Please enter a valid email. ex: johnSmith@email.com";
-    } else {
-        emailError.textContent = "";
+const form = document.querySelector('form');
+form.addEventListener('submit', e => {
+    const pass1 = document.querySelector('#password');
+    const pass2 = document.querySelector('#passwordConfirm');
+    const errorMsg = document.querySelector('.pass-error-msg');
+    if (pass1 !== pass2) {
+        e.preventDefault();
+        pass1.classList.add('error');
+        pass2.classList.add('error');
+        errorMsg.style.display = 'block';
     }
 });
